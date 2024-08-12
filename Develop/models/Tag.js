3a7c1d759c247dd77651959a158bba4f2b974bf5
tuffythejeep <tuffythejeep@yahoo.com>
2024-08-12 16:22:30 -0500

    updates to models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
@@ -7,14 +6,22 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
-
-module.exports = Tag;
